0aa2278c935aec0b789d6c50424f2ef66c1cb9c3
Keshav Bansal <keshav22bansal@gmail.com>
2020-01-19 18:33:49 +0530

    push

--- Baseline/Sub-word-LSTM/Code/char_rnn_train.py
+++ Baseline/Sub-word-LSTM/Code/char_rnn_train.py
@@ -19,35 +19,35 @@ from ekphrasis.classes.preprocessor import TextPreProcessor
 from ekphrasis.classes.tokenizer import SocialTokenizer
 from ekphrasis.dicts.emoticons import emoticons
 
-text_processor = TextPreProcessor(
-    # terms that will be normalized
-    normalize=['url', 'email', 'percent', 'money', 'phone', 'user',
-        'time', 'url', 'date', 'number'],
-    # terms that will be annotated
-    annotate={"hashtag", "allcaps", "elongated", "repeated",
-        'emphasis', 'censored'},
-    fix_html=True,  # fix HTML tokens
+# text_processor = TextPreProcessor(
+#     # terms that will be normalized
+#     normalize=['url', 'email', 'percent', 'money', 'phone', 'user',
+#         'time', 'url', 'date', 'number'],
+#     # terms that will be annotated
+#     annotate={"hashtag", "allcaps", "elongated", "repeated",
+#         'emphasis', 'censored'},
+#     fix_html=True,  # fix HTML tokens
     
-    # corpus from which the word statistics are going to be used 
-    # for word segmentation 
-    segmenter="twitter", 
+#     # corpus from which the word statistics are going to be used 
+#     # for word segmentation 
+#     segmenter="twitter", 
     
-    # corpus from which the word statistics are going to be used 
-    # for spell correction
-    corrector="twitter", 
+#     # corpus from which the word statistics are going to be used 
+#     # for spell correction
+#     corrector="twitter", 
     
-    unpack_hashtags=True,  # perform word segmentation on hashtags
-    unpack_contractions=True,  # Unpack contractions (can't -> can not)
-    spell_correct_elong=False,  # spell correction for elongated words
+#     unpack_hashtags=True,  # perform word segmentation on hashtags
+#     unpack_contractions=True,  # Unpack contractions (can't -> can not)
+#     spell_correct_elong=False,  # spell correction for elongated words
     
-    # select a tokenizer. You can use SocialTokenizer, or pass your own
-    # the tokenizer, should take as input a string and return a list of tokens
-    tokenizer=SocialTokenizer(lowercase=True).tokenize,
+#     # select a tokenizer. You can use SocialTokenizer, or pass your own
+#     # the tokenizer, should take as input a string and return a list of tokens
+#     tokenizer=SocialTokenizer(lowercase=True).tokenize,
     
-    # list of dictionaries, for replacing tokens extracted from the text,
-    # with other expressions. You can pass more than one dictionaries.
-    dicts=[emoticons]
-)
+#     # list of dictionaries, for replacing tokens extracted from the text,
+#     # with other expressions. You can pass more than one dictionaries.
+#     dicts=[emoticons]
+# )
 
 # for s in sentences:
 #     print(" ".join(text_processor.pre_process_doc(s)))
@@ -73,7 +73,7 @@ MAXLEN = 200
 
 #LSTM Model Parameters
 #Embedding
-MAX_FEATURES = 0
+MAX_FEATURES = 50
 embedding_size = 128
 # Convolution
 filter_length = 3
@@ -107,7 +107,8 @@ def parse(Masterdir,filename,seperator,datacol,labelcol,labels):
 		# Seperator for the current dataset. Currently '\t'. 
 		line = line.split(seperator)
 		#Token is the function which implements basic preprocessing as mentioned in our paper
-		tokenized_lines = text_processor.pre_process_doc(line[datacol])
+		tokenized_lines = token(line[datacol])
+		# print(tokenized_lines)
 		#Creates character lists
 		char_list = []
 		for words in tokenized_lines:
