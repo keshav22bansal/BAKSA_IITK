a98be8b3b88caac7c367c8ce9905506ab15cf826
Keshav Bansal <keshav22bansal@gmail.com>
2020-01-31 16:04:36 +0530

    data pushed

---
+++ multi_bert_sentiment/data_eng_hindi_new/analyze_output.py
@@ -0,0 +1,25 @@
+import sys
+
+f1 = open(sys.argv[1])
+f2 = open(sys.argv[2])
+f3 = open('output.txt','w')
+uid_sent_map={}
+m = {'0':'negative','1':'neutral','2':'positive'}
+for line in f2:
+    try:
+        l = line.strip().split(',')
+        uid_sent_map[l[0]] = [l[1],l[2],l[3]]
+    except:
+        pass
+count = 0
+for line in f1:
+    if count == 0:
+        count+=1
+        continue
+    count+=1
+    l = line.strip().split('\t')
+    l[2] = m[l[2]]
+    # print(l)
+    
+    l.extend(uid_sent_map[l[0]])
+    f3.write("\t".join(l)+"\n")
